165521b441599d0c7618519f148de3aedb085822
Losada, Faiber <f2losada@gmail.com>
2023-07-21 15:22:19 -0500

    se diciona proyecto

---
+++ ApiConsultantAIMaven/Controllers/ChatGPTFineTunesController1.cs
@@ -0,0 +1,55 @@
+﻿using LogicaNegocioServicio.Comunes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OpenAI_API.Completions;
+using OpenAI_API;
+using System.Threading.Tasks;
+using System;
+using ConsultantAIMavenSharedModel.Usuarios;
+using LogicaNegocioServicio.Usuarios;
+using System.Collections.Generic;
+
+namespace ApiConsultantAIMaven.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatGPTFineTunesController1 : ControllerBase
+    {
+        private readonly IConfiguration _ConnectionString;
+        public ChatGPTFineTunesController1(IConfiguration Configuration)
+        {
+            this._ConnectionString = Configuration;
+        }
+        [HttpGet("GetFineTunes")]
+        [AllowAnonymous]
+        public async Task<ActionResult<>> GetFineTunes()
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                List<Users> resultado = new List<Users>();
+                UsersDal mnu = new UsersDal(_ConnectionString.GetConnectionString("DefaultConnection"));
+                resultado = await mnu.GetAllUsers();
+                return resultado;
+
+            }
+            catch (Exception ex)
+            {
+                var emex = new ErrorDetails()
+                {
+                    StatusCode = 400,
+                    Message = "Error:  " + ex.Message.ToString()
+                };
+                return BadRequest(new JsonResult(emex));
+            }
+
+        }
+
+    }
+}
---
+++ ConsultantAIMavenSharedModel/FineTune/DataFineTune.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntregasLogyTechSharedModel.FineTune
+{
+    internal class DataFineTune
+    {
+        [Required(ErrorMessage = "El id Perfil es obligatorio")]
+        public string Prompt { get; set; }
+        [Required(ErrorMessage = "El id Perfil es obligatorio")]
+        public string completion { get; set; }
+      
+        public DataFineTune()
+        {
+            Prompt =string.Empty;
+        }
+    }
+}
